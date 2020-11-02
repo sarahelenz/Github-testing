bb7b182983baf48d7f2e4b4aeacc995a591e68fe
Sarah Elenz <selenz3112@stu.d214.org>
2020-11-02 16:07:16 -0500

    Added label, button, and textfield to storyboard, added code to display the data from textfield to the label

--- Github testing/ViewController.swift
+++ Github testing/ViewController.swift
@@ -10,11 +10,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var textfield: UITextField!
+    
+   
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+      
     }
 
-
+    @IBAction func whenButtonPressed(_ sender: Any) {
+        let data = textfield.text!
+        label.text = data
+    }
+    
 }
 
